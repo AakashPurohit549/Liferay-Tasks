1da509aea5751415fce885d9021a0d2e198c038c
Aakash Purohit <aakash.purohit.ignek@gmail.com>
2025-06-13 14:43:34 +0530

    Constants Keys added

---
+++ EntityCount/src/main/java/com/liferay/portlet/constants/EntityCountPortletKeys.java
@@ -0,0 +1,22 @@
+package com.liferay.portlet.constants;
+
+/**
+ * @author ignek
+ */
+public class EntityCountPortletKeys {
+
+	public static final String ENTITYCOUNT =
+		"com_liferay_portlet_mvcportlet_EntityCountPortlet";
+	
+	public static final String SHOW_ARTICLES =
+			"showJournalArticles";
+	
+	public static final String SHOW_DOCUMENTS =
+			"showDocuments";
+	
+	public static final String SHOW_USERS =
+			"showUsers";
+	
+	public static final String CONFIGURATION_ID =
+			"com.liferay.portlet.configuration.EntityConfiguration";
+}
