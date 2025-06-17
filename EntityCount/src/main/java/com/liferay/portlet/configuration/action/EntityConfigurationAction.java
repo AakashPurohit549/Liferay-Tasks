2ea20ff769a4065ece677283df6743c18931a0c3
Aakash Purohit <aakash.purohit.ignek@gmail.com>
2025-06-17 18:18:42 +0530

    Changes Implemented Categories and Employees added

--- EntityCount/src/main/java/com/liferay/portlet/configuration/action/EntityConfigurationAction.java
+++ EntityCount/src/main/java/com/liferay/portlet/configuration/action/EntityConfigurationAction.java
@@ -1,4 +1,5 @@
 package com.liferay.portlet.configuration.action;
+
 import com.liferay.portal.kernel.portlet.ConfigurationAction;
 import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -13,38 +14,36 @@ import javax.servlet.http.HttpServletResponse;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 
-@Component(
-	    configurationPid = EntityCountPortletKeys.CONFIGURATION_ID,
-	    configurationPolicy = ConfigurationPolicy.OPTIONAL,
-	    immediate = true,
-	    property = {
-	    		"javax.portlet.name=" + EntityCountPortletKeys.ENTITYCOUNT,	    },
-	    service = ConfigurationAction.class
-	)
-	public class EntityConfigurationAction extends DefaultConfigurationAction {
-		
-	    @Override
-	    public void processAction(PortletConfig portletConfig, ActionRequest actionRequest,
-	                               ActionResponse actionResponse) throws Exception {
+@Component(configurationPid = EntityCountPortletKeys.CONFIGURATION_ID, configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true, property = {
+		"javax.portlet.name=" + EntityCountPortletKeys.ENTITYCOUNT, }, service = ConfigurationAction.class)
+public class EntityConfigurationAction extends DefaultConfigurationAction {
 
-	        String showJournalArticles = ParamUtil.getString(actionRequest, EntityCountPortletKeys.SHOW_ARTICLES, "false");
-	        String showDocuments = ParamUtil.getString(actionRequest, EntityCountPortletKeys.SHOW_DOCUMENTS, "false");
-	        String showUsers = ParamUtil.getString(actionRequest, EntityCountPortletKeys.SHOW_USERS, "false");
+	@Override
+	public void processAction(PortletConfig portletConfig, ActionRequest actionRequest, ActionResponse actionResponse)
+			throws Exception {
 
-	        setPreference(actionRequest, EntityCountPortletKeys.SHOW_ARTICLES, showJournalArticles);
-	        setPreference(actionRequest, EntityCountPortletKeys.SHOW_DOCUMENTS, showDocuments);
-	        setPreference(actionRequest, EntityCountPortletKeys.SHOW_USERS, showUsers);
+		String displayType = ParamUtil.get(actionRequest, "displayType", "users");
 
-	        super.processAction(portletConfig, actionRequest, actionResponse);
-	    }
-	    
-	    
-	    @Override
-	    public void include(PortletConfig portletConfig, HttpServletRequest request,
-	                        HttpServletResponse response) throws Exception {
+		boolean showUsers = displayType.equals("Users");
+		boolean showImages = displayType.equals("Images");
+		boolean showArticles = displayType.equals("Artilces");
+		boolean showEmployeesCount = displayType.equals("Employees");
+		boolean showCategoiresCount = displayType.equals("Categories");
 
-	        super.include(portletConfig, request, response);
-	    }
+		setPreference(actionRequest, EntityCountPortletKeys.SHOW_ARTICLES, String.valueOf(showArticles));
+		setPreference(actionRequest, EntityCountPortletKeys.SHOW_IMAGES, String.valueOf(showImages));
+		setPreference(actionRequest, EntityCountPortletKeys.SHOW_USERS, String.valueOf(showUsers));
+		setPreference(actionRequest, EntityCountPortletKeys.SHOW_EMPLOYEES, String.valueOf(showEmployeesCount));
+		setPreference(actionRequest, EntityCountPortletKeys.SHOW_CATEGORIES, String.valueOf(showCategoiresCount));
 
+		super.processAction(portletConfig, actionRequest, actionResponse);
 	}
 
+	@Override
+	public void include(PortletConfig portletConfig, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+		super.include(portletConfig, request, response);
+	}
+
+}
