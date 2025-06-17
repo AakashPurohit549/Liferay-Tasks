2d5a04f22d3b854fd610b304118a6cc24ed4d33c
Aakash Purohit <aakash.purohit.ignek@gmail.com>
2025-06-17 18:21:54 +0530

    New constants variable added

--- EntityCount/src/main/java/com/liferay/portlet/constants/EntityCountPortletKeys.java
+++ EntityCount/src/main/java/com/liferay/portlet/constants/EntityCountPortletKeys.java
@@ -5,18 +5,17 @@ package com.liferay.portlet.constants;
  */
 public class EntityCountPortletKeys {
 
-	public static final String ENTITYCOUNT =
-		"com_liferay_portlet_mvcportlet_EntityCountPortlet";
-	
-	public static final String SHOW_ARTICLES =
-			"showJournalArticles";
-	
-	public static final String SHOW_DOCUMENTS =
-			"showDocuments";
-	
-	public static final String SHOW_USERS =
-			"showUsers";
-	
-	public static final String CONFIGURATION_ID =
-			"com.liferay.portlet.configuration.EntityConfiguration";
+	public static final String ENTITYCOUNT = "com_liferay_portlet_mvcportlet_EntityCountPortlet";
+
+	public static final String SHOW_ARTICLES = "showJournalArticles";
+
+	public static final String SHOW_IMAGES = "showImages";
+
+	public static final String SHOW_USERS = "showUsers";
+
+	public static final String SHOW_EMPLOYEES = "showEmployees";
+
+	public static final String SHOW_CATEGORIES = "showCategories";
+
+	public static final String CONFIGURATION_ID = "com.liferay.portlet.configuration.EntityConfiguration";
 }
