67027d4038f23f6da524ee3e883386b048d8613b
Aakash Purohit <aakash.purohit.ignek@gmail.com>
2025-07-08 18:38:16 +0530

    Constants class added

---
+++ employee-web/src/main/java/employee/web/constants/EmployeeWebPortletKeys.java
@@ -0,0 +1,12 @@
+package employee.web.constants;
+
+/**
+ * @author ignek
+ */
+public class EmployeeWebPortletKeys {
+
+	public static final String EMPLOYEEWEB =
+		"employee_web_EmployeeWebPortlet";
+	
+
+}
